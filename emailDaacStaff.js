ec7637cdd4b37c1faae760b9bfeeaff76a7c437b
Dunivan Stahl, Chelsey K <dunivanck@ornl.gov>
2024-07-22 10:12:37 -0400

    Sample email action

---
+++ emailDaacStaff.js
@@ -0,0 +1,46 @@
+const MessageUtil = require('message-util');
+const DatabaseUtil = require('database-util');
+
+const getCustomEmailDaacTemplate = (params, envUrl) => {
+    const text = `Hello ${params.user.name},\n\nThe submission ${params.eventMessage.submission_name} has reached the email DAAC step of the ${params.eventMessage.workflow_name}.`;
+    const html = `
+      <html>
+         <body>
+             <style>td h1 { margin: 0; padding: 0; font-size: 22px; }</style>
+             <table border="0" cellpadding="10" cellspacing="0" style="width:100%">
+               <tr style="width:100%;background:#f8f8f8">
+                   <td>
+                     <table>
+                       <tr>
+                         <td width="60"><img src = "${envUrl}/dashboard/images/app/src/assets/images/nasa-logo.78fcba4d9325e8ac5a2e15699d035ee0.svg"></td>
+                         <td><h4>Earthdata Pub</h4></td>
+                       </tr>
+                     </table>
+                   </td>
+                   <td></td>
+               </tr>
+               <tr>
+                 <td colspan="2" style="padding:20px;">
+                   <h1>Hello ${params.user.name},</h1><br><br>
+                   <p>The submission ${params.eventMessage.submission_name} has reached the email DAAC step of the ${params.eventMessage.workflow_name}.</p>
+                   <br><br>
+                   <p><a style="text-align: left;" href="${envUrl}/dashboard" aria-label="Visit Earthdata Pub Dashboard">${envUrl}/dashboard</a></p>
+                 </td>
+               </tr>
+            </table>
+         </body>
+       </html> 
+      `;
+    return [text, html];
+  };
+
+
+async function execute({eventMessage}) {
+
+    let users = await DatabaseUtil.user.findAll({group_id:'89816689-5375-4c81-a30c-bf6ed12d30fb', role_id:'a5b4947a-67d2-434e-9889-59c2fad39676'});
+
+    MessageUtil.message-util.sendEmail(users, eventMessage, getCustomEmailDaacTemplate);
+    
+}
+
+module.exports.execute = execute;
